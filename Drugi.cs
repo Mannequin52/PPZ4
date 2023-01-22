749ba254b6e65dcb2029f1af3e1709a84f95e775
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-01-22 19:06:33 +0100

    Add files via upload

---
+++ Drugi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPZ3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n4.2");
+            int[] arr142 = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] arr242 = { 3, 2, 8, 5, 5, 7 };
+            int x42, count = 0;
+            if (arr142.Length < arr242.Length)
+                x42 = arr142.Length;
+            else
+                x42 = arr242.Length;
+            for (int i = 0; i < x42; i++)
+                if (arr142[i] == arr242[i])
+                    count++;
+            Console.WriteLine("Broj zajedničkih članova je {0}", count);
+
+            Console.ReadKey();
+        }
+
+    }
+}
+    }
+}
---
+++ Prvi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPZ3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n4.1");
+            int[] arr1 = new int[10];
+            int[] arr2 = new int[10];
+            string s41;
+            int x41, n1 = 0, n2 = 0;
+            do
+            {
+                Console.WriteLine("Upišite broj:");
+                s41 = Console.ReadLine();
+                x41 = Convert.ToInt32(s41);
+                if (x41 % 2 != 0)
+                    arr1[n1++] = x41;
+                else
+                    arr2[n2++] = x41;
+            } while (x41 != 0 && n1 < 10 && n2 < 10);
+            Console.WriteLine();
+            for (int i = 0; i < n1; i++)
+                Console.WriteLine(arr1[i]);
+            Console.WriteLine();
+            for (int i = 0; i < n2; i++)
+                Console.WriteLine(arr2[i]);
+        }
+    }
+}
